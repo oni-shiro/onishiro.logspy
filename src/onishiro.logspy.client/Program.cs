5688a78343d71cc2036673601e220b3192dd4bfb
oni-shiro <sinchan.saha99@gmail.com>
2025-06-29 20:10:40 +0530

    cleanup

--- src/onishiro.logspy.client/Program.cs
+++ src/onishiro.logspy.client/Program.cs
@@ -5,6 +5,7 @@ namespace onishiro.logspy.client;
 
 sealed class Program
 {
+
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
